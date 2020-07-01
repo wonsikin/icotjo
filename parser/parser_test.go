8b3a75b81ab6067f60fe922e6d7709f1d0c8eaf9
Arthur Wang <arthur.wang@grabtaxi.com>
2020-07-01 17:43:12 +0800

    test: update test case

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,17 +1,37 @@
 package parser
 
 import (
-	"encoding/json"
+	"reflect"
 	"testing"
 )
 
-func TestMapJson(t *testing.T) {
-	mapList := map[string]string{"用户名": "User,name", "密码": "Password"}
-
-	data, err := json.Marshal(mapList)
-	if err != nil {
-		t.Errorf("fail %s", err)
+func Test_jsonMarshal(t *testing.T) {
+	type args struct {
+		v            interface{}
+		safeEncoding bool
 	}
-
-	t.Logf("result is %s", string(data))
+	tests := []struct {
+		name     string
+		args     args
+		wantData []byte
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		{"disable safe enconding", args{
+			v:            map[string]string{"用户名": "User,name", "密码": "Password"},
+			safeEncoding: false,
+		}, []byte(`{"密码":"Password","用户名":"User,name"}`), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotData, err := jsonMarshal(tt.args.v, tt.args.safeEncoding)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("jsonMarshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotData, tt.wantData) {
+				t.Errorf("jsonMarshal() = %v, want %v", gotData, tt.wantData)
+			}
+		})
+	}
 }
